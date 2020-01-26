5334a7e5bb6d32a7a1bae7d017583268f272c40b
Rahul kumar <rahul.kumar@hpe.com>
2020-01-26 13:55:14 +0530

    Added Promises

--- promises.js
+++ promises.js
@@ -1,6 +1,6 @@
 const posts = [
     { title: 'post one', body: 'THis is post one' },
-    {title:'post two',body:'THis is post two'}
+    { title: 'post two', body: 'THis is post two' }
 
 ];
 
@@ -13,15 +13,27 @@ function getPosts() {
         });
         document.body.innerHTML = output;
 
-        
+
     }, 1000);
 }
 
 function createPost(post) {
     // Return a Promise and promise takes callback and that callback takes 2 param
-    return new Promise((resolve,reject) => { });
-    setTimeout(() => {
-        posts.push(post);
-      //  callback();
-    }, 2000);
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            posts.push(post);
+            //  callback();
+            const error = false;
+            if (!error) {
+                resolve();
+            }
+            else {
+                reject('Error, something went wrong');
+            }
+        }, 2000);
+    });
+
+
 }
+// since it returns promise ,means we can use .then syntax
+createPost({ title: 'post three', body: 'THis is post 3' }).then(getPosts);
