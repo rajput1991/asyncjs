d00e32f30dfee046f13ffa3e3d4272a78d830944
Rahul kumar <rahul.kumar@hpe.com>
2020-01-26 14:18:53 +0530

    Using async await for handling promises

--- promises.js
+++ promises.js
@@ -35,12 +35,21 @@ function createPost(post) {
 
 
 }
+
+async function init() {
+    // means we are waiting untill createPost is done before call of getPosts
+ await createPost({ title: 'post three', body: 'THis is post 3' });
+ getPosts();
+}
+
+init();
+
 // since it returns promise ,means we can use .then syntax
 // so if promise is resolved , then it calls getPosts
 //createPost({ title: 'post three', body: 'THis is post 3' }).then(getPosts).catch(err=>console.log(err));
-const promise1 = Promise.resolve('Hello world');
-const promise2 = 10;
-const promise3 = new Promise((resolve, reject) => setTimeout(resolve, 2000, 'good bye'));
-Promise.all([promise1, promise2, promise3]).then((values) => console.log(values));
+// const promise1 = Promise.resolve('Hello world');
+// const promise2 = 10;
+// const promise3 = new Promise((resolve, reject) => setTimeout(resolve, 2000, 'good bye'));
+// Promise.all([promise1, promise2, promise3]).then((values) => console.log(values));
 
 
